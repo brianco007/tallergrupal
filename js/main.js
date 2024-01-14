cf13986909f4f7433616210c95b2e19c50b4625e
Brian Cordoba <briancormin@gmail.com>
2024-01-14 12:07:32 -0500

    Avance en la lógica del programa

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log('Hola desde Typescript');
+//Member input and btn
+const $memberName = document.getElementById('memberName');
+const $addBtn = document.getElementById('addBtn');
+const $listOfMembers = document.getElementById('listOfMembers');
+//Number of groups input and btn
+const $groupsNumber = document.getElementById('groupsNumber');
+const $createBtn = document.getElementById('createBtn');
+const $groups = document.getElementById('groups');
+let membersToRender = "";
+let totalParticipants = 0;
+let membersArray = [];
+$addBtn.addEventListener('click', () => {
+    totalParticipants++;
+    membersArray.push($memberName.value);
+    membersToRender += `<li> ${$memberName.value} </li>`;
+    $memberName.value = "";
+    $listOfMembers.innerHTML = membersToRender;
+    console.log(totalParticipants);
+    console.log(membersArray);
+});
+$createBtn.addEventListener('click', () => {
+    const participantsPerGroup = membersArray.length / parseInt($groupsNumber.value);
+    let groupToRender = "";
+    // for(let i = 0; i < Math.floor(parseInt($groupsNumber.value)); i++){
+    for (let i = 0; i < participantsPerGroup; i++) {
+        const randomNumber = Math.floor(Math.random() * membersArray.length);
+        groupToRender += `<li>${membersArray[randomNumber]}</li>`;
+        membersArray.splice(randomNumber, 1);
+    }
+    $groups.innerHTML = groupToRender;
+    // }
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,41 @@
-console.log('Hola desde Typescript');
+//Member input and btn
+const $memberName = document.getElementById('memberName') as HTMLInputElement
+const $addBtn = document.getElementById('addBtn') as HTMLButtonElement
+const $listOfMembers = document.getElementById('listOfMembers') as HTMLElement
+//Number of groups input and btn
+const $groupsNumber = document.getElementById('groupsNumber') as HTMLInputElement
+const $createBtn = document.getElementById('createBtn') as HTMLButtonElement
+const $groups = document.getElementById('groups') as HTMLElement
+
+
+
+let membersToRender: string = "";
+let totalParticipants:number = 0;
+let membersArray: string[] = [];
+
+
+$addBtn.addEventListener('click', ()=>{
+  totalParticipants++
+  membersArray.push($memberName.value)
+  membersToRender += `<li> ${$memberName.value} </li>`
+  $memberName.value = ""
+  $listOfMembers.innerHTML = membersToRender
+  console.log(totalParticipants)
+  console.log(membersArray)
+})
+
+
+$createBtn.addEventListener('click', ()=>{
+  const participantsPerGroup: number = membersArray.length / parseInt($groupsNumber.value)
+  
+  let groupToRender: string = ""
+  // for(let i = 0; i < Math.floor(parseInt($groupsNumber.value)); i++){
+    
+    for(let i=0; i<participantsPerGroup; i++ ){
+      const randomNumber: number = Math.floor(Math.random() * membersArray.length)
+      groupToRender += `<li>${membersArray[randomNumber]}</li>`
+      membersArray.splice(randomNumber, 1)
+    }
+    $groups.innerHTML = groupToRender
+  // }
+})
